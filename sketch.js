0b62efcd55bb38dbc42b4c92f0121f1e9816ba10
Daivik <daivik@icloud.com>
2021-06-24 10:54:36 -0400

    done

--- sketch.js
+++ sketch.js
@@ -1,16 +1,14 @@
-var bgImg;
+var bg,bgImg;
 var cat,catImg1,catImg2,catImg3,catImg4,runningCat;
-var mouse,mouseImg1,mouseImg2,mouseImg3,mouseImg4,laughingmouse;
+var mouse,mouseImg1,mouseImg2,mouseImg3,mouseImg4,laughingMouse;
 function preload() {
     //load the images here
     bgImg = loadImage("images/garden.png");
-    catImg1 = loadImage("images/cat1.png");
-
-    runningCat=loadAnimation("images/cat2.png","images/cat3.png")
+    catImg1 = loadAnimation("images/cat1.png");
+    runningCat=loadAnimation("images/cat2.png","images/cat3.png");
     catImg4 = loadImage("images/cat4.png");
-    mouseImg1 = loadImage("images/mouse1.png");
-    mouseImg2 = loadImage("images/mouse2.png");
-    mouseImg3 = loadImage("images/mouse3.png");
+    mouseImg1 = loadAnimation("images/mouse1.png");
+    laughingMouse=loadAnimation("images/mouse2.png","images/mouse3.png");
     mouseImg4 = loadImage("images/mouse4.png");
 
 
@@ -19,7 +17,20 @@ function preload() {
 function setup(){
     createCanvas(1000,800);
     //create tom and jerry sprites here
-    runningCat = createSprite(20,400,50,70);
+    bg=createSprite(500,500,100,100);
+    bg.addImage("backround",bgImg);
+
+    cat = createSprite(870,660,50,70);
+    cat.addAnimation("Mrcat",catImg1);
+    cat.scale=0.19;
+
+    mouse = createSprite(120,660);
+    mouse.addAnimation("MrMouse",mouseImg1);
+    mouse.scale=0.19;
+
+
+
+
     
 
 
@@ -30,6 +41,15 @@ function draw() {
     background(255);
     //Write condition here to evalute if tom and jerry collide
 
+    if(cat.x - mouse.x < (cat.width - mouse.width)/2){
+        cat.velocityX=0;
+        mouse.addAnimation("mouseimage", mouseImg4);
+        mouse.changeAnimation("mouseimage");
+        cat.addAnimation("catimage4", catImg4);
+        cat.changeAnimation("catimage4");
+
+    }
+
     drawSprites();
     console.log(frameCount);
    
@@ -39,6 +59,21 @@ function draw() {
 function keyPressed(){
 
   //For moving and changing animation write code here
+
+   if(keyCode === LEFT_ARROW){
+    mouse.addAnimation("Jerry",laughingMouse);
+    mouse.changeAnimation("Jerry");
+    mouse.frameDelay = 25;
+   }
+
+   
+   if(keyCode === RIGHT_ARROW){
+    cat.addAnimation("Tom",runningCat);
+    cat.changeAnimation("Tom");
+    cat.frameDelay = 25;
+    cat.velocityX=-2;
+   }
+
 
 
 }
