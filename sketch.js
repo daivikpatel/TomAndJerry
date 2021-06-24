58a0c32a4d6e5cb29e2e7026a44936c471a9c514
Daivik <daivik@icloud.com>
2021-06-23 21:47:38 -0400

    updates

--- sketch.js
+++ sketch.js
@@ -1,12 +1,27 @@
 var bgImg;
-var catImg;
+var cat,catImg1,catImg2,catImg3,catImg4,runningCat;
+var mouse,mouseImg1,mouseImg2,mouseImg3,mouseImg4,laughingmouse;
 function preload() {
     //load the images here
+    bgImg = loadImage("images/garden.png");
+    catImg1 = loadImage("images/cat1.png");
+
+    runningCat=loadAnimation("images/cat2.png","images/cat3.png")
+    catImg4 = loadImage("images/cat4.png");
+    mouseImg1 = loadImage("images/mouse1.png");
+    mouseImg2 = loadImage("images/mouse2.png");
+    mouseImg3 = loadImage("images/mouse3.png");
+    mouseImg4 = loadImage("images/mouse4.png");
+
+
 }
 
 function setup(){
     createCanvas(1000,800);
     //create tom and jerry sprites here
+    runningCat = createSprite(20,400,50,70);
+    
+
 
 }
 
@@ -16,6 +31,8 @@ function draw() {
     //Write condition here to evalute if tom and jerry collide
 
     drawSprites();
+    console.log(frameCount);
+   
 }
 
 
@@ -25,3 +42,5 @@ function keyPressed(){
 
 
 }
+
+
